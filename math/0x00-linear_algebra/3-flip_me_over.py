5b9478ffed7826e968b23f5b6037ac03a3a80da0
Tukson <santo17338@gmail.com>
2022-07-27 12:39:06 -0300

    adding comments to exercice 3

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,3 +1,12 @@
-import numpy as np
+'''Write a function def matrix_transpose(matrix): that returns the transpose of a 2D matrix, matrix'''
 def matrix_transpose(matrix):
-    return np.array(matrix).transpose().tolist()
+    '''this function transpose a matrix'''
+    width = len(matrix[0])
+    height = len(matrix)
+    new_mat = []
+    for i in range(width):
+        new_mat2 = []
+        for block in matrix:
+            new_mat2.append(block[i])
+        new_mat.append(new_mat2)
+    return new_mat
