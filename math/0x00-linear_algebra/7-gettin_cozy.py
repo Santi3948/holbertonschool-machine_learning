f87268f466119bccf62f52fc75c796df68072012
Tukson <santo17338@gmail.com>
2022-07-27 14:00:23 -0300

    adding comment to module and functions ex 7

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,7 +1,20 @@
-import numpy as np
+#!/usr/bin/env python3
+'''Write a function that concatenates two matrices along a specific axis'''
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
-    try:
-        arr_concaxis = np.concatenate((mat1, mat2), axis)
-    except:
+    '''concatenates two matrices along a specific axis'''
+    if axis not in [0, 1]:
         return None
-    return arr_concaxis.tolist()
+    aux = list(map(list, mat1))
+    if axis not in [0, 1]:
+        return None
+    if axis == 0:
+        for item in mat2:
+            aux.append(item)
+    elif axis == 1:
+        i = 0
+        for item in aux:
+            item += mat2[i]
+            i += 1
+    return aux
