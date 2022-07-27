9c5f3f68aa8486d202d725d75ee84e443d1928dc
Tukson <santo17338@gmail.com>
2022-07-27 13:57:10 -0300

    adding comment to module and fucntions ex 8

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,6 +1,26 @@
-import numpy as np
+#!/usr/bin/env python3
+'''Write a function that performs matrix multiplication'''
+
+
 def mat_mul(mat1, mat2):
-    if np.shape(mat1)[1] != np.shape(mat2)[0]:
+    '''matrix multiplication'''
+    if len(mat1[0]) != len(mat2):
         return None
-    mat_mul_arr = np.matmul(mat1, mat2)
-    return mat_mul_arr.tolist()
+    width = len(mat2[0])
+    height = len(mat2)
+    new_mat = []
+    for i in range(width):
+        new_mat2 = []
+        for block in mat2:
+            new_mat2.append(block[i])
+        new_mat.append(new_mat2)
+    mul = []
+    for item1 in mat1:
+        aux = []
+        for item2 in new_mat:
+            x = 0
+            for i in range(len(mat1[0])):
+                x += item1[i] * item2[i]
+            aux.append(x)
+        mul.append(aux)
+    return mul
