01420a4bac6b11197cd45da77338a0493f317a0e
Tukson <santo17338@gmail.com>
2022-07-27 13:31:00 -0300

    adding comment to module and function ex 12

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+'''Write a function that performs element-wise add, sub, mul, and div:'''
+
+
 def np_elementwise(mat1, mat2):
+    '''addition, subtraction, multiplication, and division'''
     return (sum, mat1 - mat2, mat1 * mat2, mat1 / mat2)
