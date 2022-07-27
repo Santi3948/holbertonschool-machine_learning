2b923f22b79b85ab24cb6ccb465df76b286584ad
Tukson <santo17338@gmail.com>
2022-07-27 13:32:32 -0300

    changing return ex 12

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -4,4 +4,4 @@
 
 def np_elementwise(mat1, mat2):
     '''addition, subtraction, multiplication, and division'''
-    return (sum, mat1 - mat2, mat1 * mat2, mat1 / mat2)
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
