75674f9f44e82d3bb324ec7fa0396f95ba67e341
Tukson <santo17338@gmail.com>
2022-07-27 14:28:18 -0300

    changing border case ex 7

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,12 +5,16 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     '''concatenates two matrices along a specific axis'''
     if axis not in [0, 1]:
-        return 6
+        return None
     aux = list(map(list, mat1))
     if axis == 0:
+        if len(mat2[0]) != len(mat1[0]):
+            return None
         for item in mat2:
             aux.append(item)
     elif axis == 1:
+        if len(mat2) != len(mat1):
+            return None
         i = 0
         for item in aux:
             item += mat2[i]
