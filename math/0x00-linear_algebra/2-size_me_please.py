f196cf14e16fb6712edd4417189ffe02681bbdbe
Tukson <santo17338@gmail.com>
2022-07-27 14:37:46 -0300

    add ex 2

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,4 +1,11 @@
-import numpy as np
+#!/usr/bin/env python3
+'''Write a function that calculates the shape of a matrix'''
+
+
 def matrix_shape(matrix):
-    arr = np.array(matrix)
-    return  list(arr.shape)
+    '''this function calculates the matrix shape'''
+    mat_shape = []
+    while type(matrix) is list:
+        mat_shape.append(len(matrix))
+        matrix = matrix[0]
+    return mat_shape
