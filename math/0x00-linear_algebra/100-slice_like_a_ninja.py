d83613536eac1422451ad0fa068a391168d98d43
Tukson <santo17338@gmail.com>
2022-07-29 17:46:07 -0300

    100 add

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -2,7 +2,19 @@
 '''Write a function that slices a matrix along specific axes:'''
 
 def np_slice(matrix, axes={}):
+    dim = matrix.ndim
+    print(f'{dim} dimension')
+    new = matrix.copy()
     '''slice the matrix'''
-    xd = matrix.take(indices=range(1, 3), axis=1)
-    
-    return xd
+    for item in sorted(axes):
+        print(f'{item} axes')
+        aux = matrix.copy()
+        for item2 in range(0, sorted(axes)[len(sorted(axes)) - 1] + 1):
+            print(f'{item2} order axes')
+            if item2 == item:
+                if item2 == (dim - 1):
+                    print(f'aaaaaa {aux} que lokkkeee')
+                else:
+                    print("no soy dimension")
+            aux = aux[0]
+    return new
