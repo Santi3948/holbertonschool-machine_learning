d6f1712c01bb08831fce6e10667adf2510049b0e
Tukson <santipane17@gmail.com>
2022-08-09 18:44:13 -0300

    adding heron func and fixing errors on init

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -13,13 +13,27 @@ class Normal:
                 raise ValueError("data must contain multiple values")
             i = 0
             aux = 0
+            aux2 = 0
             for item in data:
                 i += 1
                 aux += item
-            self.mean = float(mean)
-            self.stddev = float(stddev)
+            self.mean = float(aux/i)
+            for item2 in data:
+                aux2 += (item2 - self.mean) ** 2
+            self.stddev = float(self.heron(aux2/i))
         else:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
             self.mean = float(mean)
             self.stddev = float(stddev)
+
+    def heron(self, n):
+        """heron sequence"""
+        prev = 0
+        a = n / 2
+        while True:
+            a = (1/2) * (a + (n / a))
+            if a == prev:
+                break
+            prev = a
+        return float(a)
