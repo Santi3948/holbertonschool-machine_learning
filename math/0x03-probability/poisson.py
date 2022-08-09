8e099011d9e34c4d7f392779c2d8ccfb221e6222
Tukson <santipane17@gmail.com>
2022-08-09 16:19:00 -0300

    adding if conditions to cdf

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -44,6 +44,10 @@ class Poisson:
 
     def cdf(self, k):
         """Calculates the value of the CDF for a given number of “successes”"""
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
         aux = 0
         for i in range(k+1):
             aux += self.pmf(i)
