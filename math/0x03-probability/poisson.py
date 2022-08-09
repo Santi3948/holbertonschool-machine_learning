f54f03ce4656da28dc13b9a6e3521d6ea1af88f3
Tukson <santipane17@gmail.com>
2022-08-09 16:03:32 -0300

    adding data != None condition on init

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -9,7 +9,7 @@ class Poisson:
 
     def __init__(self, data=None, lambtha=1.):
         """Class contructor"""
-        if data:
+        if data or data != None:
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
@@ -26,17 +26,22 @@ class Poisson:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
-    
+
     def factorial(self, x):
         """factorial function"""
         if x == 1:
             return 1
         return (self.factorial(x - 1) * x)
-    
+
     def pmf(self, k):
-        """Class contructor"""
+        """Calculates the value of the PMF for a given number of “successes”"""
         if type(k) is not int:
             k = int(k)
         if k < 0:
             return 0
-        return ((self.e ** (-self.lambtha)) * ((self.lambtha) ** k)/self.factorial(k))
+        return ((self.e ** (-self.lambtha)) * ((self.lambtha) ** k)
+                / self.factorial(k))
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of “successes”"""
+        return 1
