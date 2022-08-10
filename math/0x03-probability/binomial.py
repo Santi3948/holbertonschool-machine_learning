1a2449e8765571f83144ae1794c01e703efaf3c3
Tukson <santipane17@gmail.com>
2022-08-10 18:01:22 -0300

    change if condition binomisl

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -28,7 +28,7 @@ class Binomial:
         else:
             if n <= 0:
                 raise ValueError("n must be a positive value")
-            if p < 0 or p > 1:
+            if p <= 0 or p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
             self.n = n
             self.p = p
