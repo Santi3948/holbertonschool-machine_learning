51d81ff62feb1a7c98c83b6fd15be23cc73d34c4
Tukson <santipane@gmail.com>
2022-08-16 18:12:40 -0300

    first commit supervised learning all works locally

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import numpy as np
+"""Write a class Neuron that defines a single neuron performing binary classification"""
+
+
+class Neuron:
+    """neuron performing binary classification"""
+    def __init__(self, nx):
+        """class constructor"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__b = 0
+        self.__A = 0
+        self.__W = np.random.randn(1, nx)
+
+    @property
+    def W(self):
+        """getter _W"""
+        return self.__W
+
+    @property
+    def A(self):
+        """getter _A"""
+        return self.__A
+
+    @property
+    def b(self):
+        """getter _b"""
+        return self.__b
