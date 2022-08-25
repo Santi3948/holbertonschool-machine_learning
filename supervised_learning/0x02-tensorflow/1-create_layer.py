12b7fb0666145f9f68f880b2a55ac8145efac579
Tukson <santipane@gmail.com>
2022-08-25 16:14:15 -0300

    adding kernel initializer to create layer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -5,8 +5,8 @@ import tensorflow.compat.v1 as tf
 
 def create_layer(prev, n, activation):
     """returns two placeholders, x and y, for the neural network:"""
-    linear_model = tf.layers.Dense(name="layer", units=n,
-                                   activation=activation)
-    y = linear_model(prev)
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    linear_model = tf.layers.Dense(name="layer", units=n,
+                                   activation=activation, kernel_initializer=init)
+    y = linear_model(prev)
     return y
