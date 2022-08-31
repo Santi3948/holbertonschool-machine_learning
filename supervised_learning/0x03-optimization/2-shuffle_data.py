5313a1dd4aec9df8bb9d0813ec58d455eee5d110
Tukson <santo17338@gmail.com>
2022-08-31 14:46:49 -0300

    fixing order on shuffle

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -5,4 +5,7 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """shuffles the data points in two matrices the same way"""
-    return np.random.permutation(X), np.random.permutation(Y)
+    shuffler = np.random.permutation(len(X))
+    x_shuffled = X[shuffler]
+    y_shuffled = Y[shuffler]
+    return x_shuffled, y_shuffled
