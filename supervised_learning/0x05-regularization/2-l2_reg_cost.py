a6ade87a924a2d2fccd9f3bded0b8c18b6f248f9
Tukson <santipane@gmail.com>
2022-09-08 22:32:44 -0300

    tf compat v1

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """regularization of a model"""
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 def l2_reg_cost(cost):
     """calculates the cost of a neural network L2
     """
-    return cost + tf.losses.get_regularization_losses()
+    return cost + tf.compat.v1.losses.get_regularization_loss()
